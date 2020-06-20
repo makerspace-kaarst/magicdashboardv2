414d1d865cc1f170c1d50fc737a82255ed4d396a
Stephan Wiechers <stephan.wiechers04@gmail.com>
2020-06-20 14:24:09 +0200

    100% new
    It's the final version., du du du du duuu

---
+++ Backend/get_routes.py
@@ -0,0 +1,58 @@
+import json
+import cache
+
+### --- SAVE GETS --- [The only ones you SHOULD have]
+
+# returns amount of nodes and grid settings
+# Rows|Columns|Node amount
+def grid_settings(*args):
+    with open("../Frontend/templates/node.html",'rb') as f:
+        node_template = f.read().decode().replace('\n','').replace('\r','').replace('\"','\'').strip()
+    return f"[{cache.read(['grid','rows'])},{cache.read(['grid','columns'])},{str(len(cache.read(['update_delay'])))},\"{node_template}\"]"
+
+### --- UNSAVE GETS --- [The ones you SHOULD NOT have]
+
+# Checks for update timers to hit 0  and returns booleans
+# for every node, describing their need for an update
+def newContentAvailable(*args):
+    # for faster access, get the DB object
+    db = cache.get_raw()
+    out = []
+    for nid,node in enumerate(db['update_timer']):
+        out.append(node <= 0 and len(db['node_html'][nid]) > 1 or db['force_update'])
+        if node <= 0:
+            db['update_timer'][nid] = db['update_delay'][nid]-1
+        else:
+            db['update_timer'][nid] -= 1
+    db['force_update'] = False
+    return json.dumps(out)
+
+
+# Reurns the current HTMl for a node, loops
+def node_html(*args,**kwargs):
+    # extract node_id from GET params
+    node_id = int(args[0]['node_id'])
+    # for faster access, get the DB object
+    db = cache.get_raw()
+    try:
+        # fetch the current html data based on the html index of the node
+        out = db['node_html'][node_id][db['html_index'][node_id]]
+        # update the html_index and roll over
+        db['html_index'][node_id] += 1
+        db['html_index'][node_id] %= len(db['node_html'][node_id])
+    except IndexError:
+        out = ""
+    return out
+
+
+# Set force update property
+def force_update(*args):
+    cache.write(['force_update'],True)
+    return "OK"
+
+
+def windowtitle(*args):
+    return cache.read(['title'])
+
+def test(*args):
+    return "You haz found the super secret mega epic GET endpoint... have a flag{wait_this_still_is_not_a_ctf_now_i_am_sad}\n\n"+str(args[0])
---
+++ Backend/post_routes.py
@@ -0,0 +1,129 @@
+import json
+import cache
+import urllib
+import json
+from os import remove as _os_remove_file
+
+### --- SETTING POSTS ---
+
+# Download a file from remote host and place it in /uploads
+def upload_file(*args):
+    if '..' in args[0]['filename']:
+        return 'Nope'
+    with open('../Frontend/uploads/'+args[0]['filename'],'wb') as f:
+        f.write(args[1])
+    return 'OK'
+
+
+# Deletes a file from /uploads
+def delete_file(*args):
+    filename = args[0]['filename']
+    if '..' in filename:  # no directory traversal
+        return "Nope"
+    try:  # Delet the fiile, bu don't tell the user abput errors, tht would enable destructive enumeration
+        _os_remove_file("../Frontend/uploads/"+filename)
+        db['force_update'] = True
+        return 'OK'
+    except FileNotFoundError:
+        pass
+        return 'File not found'
+
+
+# Adds a new node to the database
+def add_node(*args):
+    node_id = int(args[0]['node_id'])
+    db = cache.get_raw()
+    db['node_html'].insert(node_id,[])
+    db['html_index'].insert(node_id,0)
+    db['update_timer'].insert(node_id,0)
+    db['update_delay'].insert(node_id,2)
+    db['force_update'] = True
+    return 'OK'
+
+# Removes a node
+def delete_node(*args):
+    node_id = int(args[0]['node_id'])
+    db = cache.get_raw()
+    del db['node_html'][node_id]
+    del db['html_index'][node_id]
+    del db['update_timer'][node_id]
+    del db['update_delay'][node_id]
+    db['force_update'] = True
+    return 'OK'
+
+
+# Add or remove a state from a node
+def manage_node(*args):
+    node_id = int(args[0]['node_id'])
+    action = args[0]['action']
+    db = cache.get_raw()
+
+    # add new state
+    if action in ['add','insert','a','i','+']:
+        index = int(args[0]['index'])  # state index
+        html = urllib.parse.unquote(args[0]['html'])
+        db['node_html'][node_id].insert(index,html)
+        db['force_update'] = True
+
+    # remove state
+    elif action in ['remove','delete','del','r','d','-']:
+        index = int(args[0]['index'])  # state index
+        del db['node_html'][node_id][index]
+        db['force_update'] = True
+        db['html_index'][node_id] = 0
+
+    # change update delay [reset timer to prevent overflow]
+    elif action in ["speed","delay"]:
+        db['update_delay'][node_id] = abs(int(args[0]['delay']))
+        db['update_timer'][node_id] = 0
+
+    elif action in ["update","change","replace"]:
+        index = int(args[0]['index'])  # state index
+        html = urllib.parse.unquote(args[0]['html'])
+        db['node_html'][node_id][index] = html
+        db['force_update'] = True
+    else:
+        return "Invalid action"
+    return "OK"
+
+
+def update_grid(*args):
+    cache.write(['grid','rows'],int(args[0]['rows']))
+    cache.write(['grid','columns'],int(args[0]['columns']))
+    return "OK"
+
+
+# Set force update property
+def force_update(*args):
+    cache.write(['force_update'],True)
+    return "OK"
+
+
+def test(*args):
+    return "You haz found the super secret mega epic POST endpoint... have a flag{wait_this_is_not_a_ctf}\n\n"+str(args[0])
+
+
+def set_uuid(*args):
+    cache.write(['auth_uuid'],args['auth'])
+
+
+# returns a dump of the Database
+def db(*args):
+    c = cache.get_raw().copy()
+    c['config'] = ['No passwords for you, you don\'t need them anyways, right?']
+    return str(c)
+
+
+# Change the Board title
+def update_title(*args):
+    cache.write(['title'],urllib.parse.unquote(args[0]['title']))
+    return 'OK'
+
+
+# Danger zone Endpoints
+def update_password(*args):
+    temp = cache.read(['config'])
+    temp['password'] = args[0]['password']
+    cache.write(['config'],temp)
+    print(cache.read(['config']))
+    return 'OK'
